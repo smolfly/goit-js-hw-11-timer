8803774d8600129adf624d4da334ef8758d8eb51
smolfly <68849295+smolfly@users.noreply.github.com>
2021-02-17 21:22:18 +0300

    Update timer.js

--- src/timer.js
+++ src/timer.js
@@ -6,8 +6,8 @@ const refs = {
     secs: document.querySelector('span[data-value="secs"]'),
   };
   
-  const targetDate = new Date('Feb 23, 2021'); 
-  const timer = {
+const targetDate = new Date('Feb 23, 2021'); 
+const timer = {
     intervalId: null, 
     start() {
       updateClock(); 
@@ -17,47 +17,40 @@ const refs = {
       }, 1000);
     },
   };
+
   timer.start();
   
   
 function updateClock() {
-    const currentDate = Date.now(); 
-    // console.log(currentDate);
+const currentDate = Date.now(); 
 const time = targetDate - currentDate;
-    // console.log(time);
-setTime(time);
-  
-    
-if (time <= 0) {
+ if (time <= 0) {
       console.log('clear');
       clearInterval(this.intervalId);
       this.intervalId = null;
       setTime(0);
-    }
+  return;
+  }
+ setTime(time);
 }
  
 function pad(value) {
     return String(value).padStart(2, '0');
 }
   
+function newClockNumber (days, hours, mins, secs) {
+
+  refs.days.textContent = `${days}`;
+  refs.hours.textContent = `${hours}`;
+  refs.mins.textContent = `${mins}`;
+  refs.secs.textContent = `${secs}`;
+}
+
   
 function setTime(time) {
-    // оставшиеся дни: делим значение UTC на 1000 * 60 * 60 * 24, количество миллисекунд в одном дне (миллисекунды * секунды * минуты * часы)
-    const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
-  
-    //   оставшиеся часы: получаем остаток от предыдущего расчета с помощью оператора остатка % и делим его на количество миллисекунд в одном часе (1000 * 60 * 60 = миллисекунды * минуты * секунды)
-    const hours = pad(
-      Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)),
-    );
-  
-    //  оставшиеся минуты: получаем оставшиеся минуты и делим их на количество миллисекунд в одной минуте (1000 * 60 = миллисекунды * секунды)
-    const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
-  
-    //  оставшиеся секунды: получаем оставшиеся секунды и делим их на количество миллисекунд в одной секунде (1000)
-    const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
-  
-    refs.days.textContent = `${days}`;
-    refs.hours.textContent = `${hours}`;
-    refs.mins.textContent = `${mins}`;
-    refs.secs.textContent = `${secs}`;
+const days = pad(Math.floor(time / (1000 * 60 * 60 * 24)));
+const hours = pad(Math.floor((time % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)));
+const mins = pad(Math.floor((time % (1000 * 60 * 60)) / (1000 * 60)));
+const secs = pad(Math.floor((time % (1000 * 60)) / 1000));
+newClockNumber (days, hours, mins, secs);
 }
